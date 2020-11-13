d18963432b93f7371a2ec1cd61fca6cf1ed52c9b
Joe Fioresi <joefioresi718@gmail.com>
2020-11-13 15:33:18 -0500

    add event schema

---
+++ models/Event.js
@@ -0,0 +1,14 @@
+const { Int32 } = require('mongodb');
+const { model, Schema } = require('mongoose');
+
+const eventSchema = new Schema({
+	UserId: Int32,
+	StartTime: String,
+	EndTime: String,
+	Group: String,
+	// Friends: [{ id: String, Username: String, pending: Boolean }],
+});
+
+const Event = model('event', eventSchema);
+
+module.exports = Event;
